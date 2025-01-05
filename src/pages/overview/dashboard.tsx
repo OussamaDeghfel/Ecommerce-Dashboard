d6d6dbaa863e9dfda78f1c70ac61299452ce3339
OussamaDeghfel <oussama.deghfel@gmail.com>
2025-01-05 12:08:57 +0100

    implement the overview structure

---
+++ src/pages/overview/dashboard.tsx
@@ -0,0 +1,15 @@
+const Dashboard = () => {
+  return (
+    <>
+    <div>
+        <div>dashboard and timepicker</div>
+        <div>income cards</div>
+        <div>transaction</div>
+        <div>revenue</div>
+    </div>
+    <div>Analytic Visual</div>
+    </>
+  )
+}
+
+export default Dashboard
