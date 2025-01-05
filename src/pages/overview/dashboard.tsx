336fbf9d2eea3a9f951023d6441eb6229f7dda5a
OussamaDeghfel <oussama.deghfel@gmail.com>
2025-01-05 21:11:24 +0100

    add graph for the revenue income using chart library

--- src/pages/overview/dashboard.tsx
+++ src/pages/overview/dashboard.tsx
@@ -1,21 +1,24 @@
 import { CgWebsite } from "react-icons/cg";
 import Performance from "./performance";
+import Revenue from "./revenue";
 
 const Dashboard = () => {
   return (
     <>
-      <div className="w-full h-screen bg-gray-200 p-8">
+      <div className="w-full h-screen bg-gray-200 p-8 overflow-y-scroll">
         <div className="flex justify-between items-center mb-9">
           <h1 className="text-2xl font-bold">Hello, John</h1>
           <button className="flex justify-center items-center space-x-2 bg-blue-400 hover:bg-green-400 p-2 rounded-md font-medium">
             <CgWebsite color="white" /> <span className="text-white">Open Store</span>
           </button>
         </div>
-        <div>
+        <div className="space-y-8">
           <div className="w-[50%]">
             <Performance />
           </div>
-          <div>Revenue</div>
+          <div className="w-[50%]">
+            <Revenue />
+          </div>
         </div>
       </div>
     </>
---
+++ src/pages/overview/revenue.tsx
@@ -0,0 +1,70 @@
+
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  Tooltip,
+  CartesianGrid,
+  ResponsiveContainer,
+} from "recharts";
+
+const data = [
+  { month: "Jan", revenue: 170 },
+  { month: "Feb", revenue: 200 },
+  { month: "Mar", revenue: 220 },
+  { month: "Apr", revenue: 270 },
+  { month: "May", revenue: 300 },
+  { month: "Jun", revenue: 250 },
+  { month: "Jul", revenue: 200 },
+  { month: "Aug", revenue: 350 },
+  { month: "Sep", revenue: 387 },
+  { month: "Oct", revenue: 360 },
+  { month: "Nov", revenue: 380 },
+  { month: "Dec", revenue: 470 },
+];
+
+const Revenue = () => {
+  return (
+    <div>
+      <div className="flex w-full justify-between items-center">
+        {" "}
+        <span className="text-xl font-medium">Revenue</span>{" "}
+        <ul className="flex w-fit h-fit bg-gray-300 rounded-md">
+          <li className="p-2 px-4 cursor-pointer hover:bg-slate-600 hover:text-white">
+            Day
+          </li>
+        </ul>
+      </div>
+      <div className="p-4 bg-gray-200 shadow-lg rounded-md border-2 border-gray-300 mt-2">
+        <div className="space-y-2">
+          <h2 className="text-xl">Total Revenue</h2>
+          <h1 className="text-2xl font-bold">$14,020,110</h1>
+          <p className="text-gray-400">
+            You gained <span className="text-green-600">$470.00</span> this
+            month
+          </p>
+        </div>
+        <div className="mt-5">
+          <ResponsiveContainer width="100%" height={250}>
+            <LineChart data={data}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="month" />
+              <YAxis />
+              <Tooltip />
+              <Line
+                type="monotone"
+                dataKey="revenue"
+                stroke="#82ca9d"
+                strokeWidth={2}
+                dot={{ r: 4 }}
+              />
+            </LineChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Revenue;
