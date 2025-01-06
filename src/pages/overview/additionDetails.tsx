57bdcc776a9b552d022e787e7b2ad216ae61f07f
OussamaDeghfel <oussama.deghfel@gmail.com>
2025-01-06 17:38:49 +0100

    add the product details

--- src/pages/overview/additionDetails.tsx
+++ src/pages/overview/additionDetails.tsx
@@ -3,17 +3,21 @@ import { IoIosNotifications } from "react-icons/io";
 
 const AdditionDetails = () => {
   return (
-    <div>
+    <div className="flex flex-col space-y-8">
       <div className="space-y-4">
         <div className="flex justify-between items-center">
           <h1 className="text-2xl font-bold">Shop Advisor</h1>
           <button className="flex justify-center items-center space-x-2">
-            <span>See All</span> <FaArrowRight/>{" "}
+            <span>See All</span> <FaArrowRight />{" "}
           </button>
         </div>
         <div className="flex flex-col border-2 border-gray-300 rounded-md p-5">
           <div className="flex justify-center items-start space-x-4">
-            <IoIosNotifications color="green" className="bg-green-200 p-2 rounded-lg" size={40} />
+            <IoIosNotifications
+              color="green"
+              className="bg-green-200 p-2 rounded-lg"
+              size={40}
+            />
             <div className="flex flex-col space-y-2">
               <h3 className="font-medium">The Black Friday Starts Tomorrow</h3>
               <p className="text-sm text-gray-400">
@@ -22,11 +26,34 @@ const AdditionDetails = () => {
             </div>
           </div>
           <div className="w-full flex justify-end">
-            <button className="rounded-md px-2 py-1 shadow-md w-fit  border-2 border-gray-300 cursor-pointer hover:bg-green-200 hover:font-bold">Learn More</button>
+            <button className="rounded-md px-2 py-1 shadow-md w-fit  border-2 border-gray-300 cursor-pointer hover:bg-green-200 hover:font-bold">
+              Learn More
+            </button>
           </div>
         </div>
       </div>
-      <div>Products</div>
+      <div className="space-y-4">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-bold">Products</h1>
+          <button className="flex justify-center items-center space-x-2">
+            <span>See All</span> <FaArrowRight />{" "}
+          </button>
+        </div>
+        <div className="flex flex-col border-2 border-gray-300 rounded-md p-5">
+          <div className="flex justify-between items-center py-2 border-b border-gray-300">
+            <span className="font-medium">Active listings</span>
+            <span className="text-lg font-semibold text-gray-900">6</span>
+          </div>
+          <div className="flex justify-between items-center py-2 border-b border-gray-300">
+            <span className="font-medium">Expired</span>
+            <span className="text-lg font-semibold text-gray-900">2</span>
+          </div>
+          <div className="flex justify-between items-center py-2">
+            <span className="font-medium">Sold out</span>
+            <span className="text-lg font-semibold text-gray-900">5</span>
+          </div>
+        </div>
+      </div>
       <div>Recent Activities</div>
     </div>
   );
