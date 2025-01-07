315eed2c87091db447d01e8ed9bac9e4468c3581
OussamaDeghfel <oussama.deghfel@gmail.com>
2025-01-07 14:07:22 +0100

    import on the dashboard page and enhance the styling

--- src/pages/overview/dashboard.tsx
+++ src/pages/overview/dashboard.tsx
@@ -2,17 +2,21 @@ import { CgWebsite } from "react-icons/cg";
 import Performance from "./performance";
 import Revenue from "./revenue";
 import AdditionDetails from "./additionDetails";
+import PlanChoose from "./planChoose";
 
 const Dashboard = () => {
   return (
     <>
-      <div className="w-full h-screen bg-gray-200 p-8 overflow-y-scroll">
-        <div className="flex justify-between items-center mb-9">
-          <h1 className="text-2xl font-bold">Hello, John</h1>
+      <div className="w-full h-screen bg-gray-200 p-8 overflow-y-scroll space-y-6">
+        <div className="flex justify-between items-center">
+          <h1 className="text-2xl font-bold">Hello, Oussama</h1>
           <button className="flex justify-center items-center space-x-2 bg-blue-400 hover:bg-green-400 p-2 rounded-md font-medium">
             <CgWebsite color="white" />{" "}
             <span className="text-white">Open Store</span>
           </button>
+        </div>
+        <div>
+          <PlanChoose />
         </div>
         <div className="flex space-x-6">
           <div className="w-[75%] space-y-8">
--- src/pages/overview/planChoose.tsx
+++ src/pages/overview/planChoose.tsx
@@ -1,17 +1,17 @@
 import { AiOutlineCrown } from 'react-icons/ai'
 import { CgClose } from 'react-icons/cg'
-import { PiArrowElbowUpRight } from 'react-icons/pi'
+import { FiArrowUpRight } from 'react-icons/fi'
 
 const PlanChoose = () => {
   return (
-    <div className='flex justify-between items-center px-3 p-2 rounded-md bg-green-200 border-2 border-green-400'>
-        <div className='flex'>
-        <AiOutlineCrown />
-        <p>Upgrade your plan to unlock advanced features</p>
+    <div className='flex justify-between items-center px-3 p-2 rounded-md bg-green-100 border-2 border-green-300'>
+        <div className='flex justify-center items-center space-x-1'>
+        <AiOutlineCrown size={30} color='purple'/>
+        <p className='text-lg'>Upgrade your plan to unlock advanced features</p>
         </div>
-        <div className='flex '>
-            <button><span>Select Plan</span> <PiArrowElbowUpRight /> </button>
-            <button> <CgClose /> </button>
+        <div className='flex space-x-3'>
+            <button className='flex space-x-1 border-2 shadow-md border-gray-300 bg-white py-1 px-2 rounded-md justify-center items-center'><span className='text-sm'>Select Plan</span> <FiArrowUpRight /> </button>
+            <button> <CgClose size={20}/> </button>
         </div>
     </div>
   )
