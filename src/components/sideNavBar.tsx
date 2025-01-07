b7ad52c122a5f88c69e55c466666208b09ed1a27
OussamaDeghfel <oussama.deghfel@gmail.com>
2025-01-07 19:57:30 +0100

    change the menu list styling and show how the active item looks

--- src/components/sideNavBar.tsx
+++ src/components/sideNavBar.tsx
@@ -8,7 +8,7 @@ import { TiPin } from "react-icons/ti";
 
 const SideNavbar = () => {
   return (
-    <div className="flex flex-col w-[15%] h-screen bg-gray-300 border-r border-gray-500 p-8 text-black space-y-8">
+    <div className="flex flex-col w-[15%] h-screen bg-gray-300 border-r border-gray-500 px-4 py-8 text-black space-y-8">
       <h1 className="text-xl font-bold text-black">StoreTrack</h1>
       <div className="flex w-[25vh] justify-between border-2 border-gray-200 p-2 rounded-md mt-4 bg-white ">
         <div className="flex justify-center items-center space-x-1">
@@ -17,15 +17,15 @@ const SideNavbar = () => {
         </div>
         <button><RiExpandUpDownLine /></button>
       </div>
-      <div className="flex flex-col space-y-2 justify-start items-start">
+      <div className="flex  flex-col space-y-2 justify-start items-start">
         <span className="text-sm">MENU</span>
-        <ul className="flex flex-col justify-start items-start">
-          <li className="flex justify-center items-center space-x-2 px-2 py-1 "><RiHome4Line size={20} /><span className="text-lg">Home</span></li>
-          <li className="flex justify-center items-center space-x-2 px-2 py-1 "><BsGrid size={20} /><span className="text-lg">Catalog</span></li>
-          <li className="flex justify-center items-center space-x-2 px-2 py-1 "><RiMoneyDollarCircleLine size={20} /><span className="text-lg">Finances</span></li>
-          <li className="flex justify-center items-center space-x-2 px-2 py-1 "><GrGroup size={20} /><span className="text-lg">Customers</span></li>
-          <li className="flex justify-center items-center space-x-2 px-2 py-1 "><TiPin size={20} /><span className="text-lg">Marketing</span></li>
-          <li className="flex justify-center items-center space-x-2 px-2 py-1 "><IoStatsChartOutline size={20} /><span className="text-lg">Analytics</span></li>
+        <ul className="flex flex-col w-full space-y-2">
+          <li className="w-full flex justify-start items-center space-x-2 px-2 py-1 cursor-pointer bg-white shadow-sm rounded-md border-2 border-gray-350"><RiHome4Line size={20} /><span className="text-lg">Home</span></li>
+          <li className="flex justify-start items-center space-x-2 px-2 py-1 cursor-pointer"><BsGrid size={20} /><span className="text-lg">Catalog</span></li>
+          <li className="flex justify-start items-center space-x-2 px-2 py-1 cursor-pointer"><RiMoneyDollarCircleLine size={20} /><span className="text-lg">Finances</span></li>
+          <li className="flex justify-start items-center space-x-2 px-2 py-1 cursor-pointer"><GrGroup size={20} /><span className="text-lg">Customers</span></li>
+          <li className="flex justify-start items-center space-x-2 px-2 py-1 cursor-pointer"><TiPin size={20} /><span className="text-lg">Marketing</span></li>
+          <li className="flex justify-start items-center space-x-2 px-2 py-1 cursor-pointer"><IoStatsChartOutline size={20} /><span className="text-lg">Analytics</span></li>
         </ul>
       </div>
     </div>
