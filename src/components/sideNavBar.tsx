cb21a305f518ff3f0ad02c37762710ce52b5a726
OussamaDeghfel <oussama.deghfel@gmail.com>
2025-01-11 21:02:23 +0100

    add select option for multiple stores, refactor the sidebar navigation menu

--- src/components/sideNavBar.tsx
+++ src/components/sideNavBar.tsx
@@ -1,16 +1,13 @@
 import { GrGroup } from "react-icons/gr";
 import { IoStatsChartOutline, IoStorefront } from "react-icons/io5";
-import {
-  RiExpandUpDownLine,
-  RiHome4Line,
-  RiMoneyDollarCircleLine,
-} from "react-icons/ri";
+import { RiHome4Line, RiMoneyDollarCircleLine } from "react-icons/ri";
 
 import ownerImage from "../assets/ownerimage.webp";
 import { MdOutlineSettings } from "react-icons/md";
 import { BiLogOutCircle } from "react-icons/bi";
 import { Link } from "react-router";
 import { useState } from "react";
+import { Select } from "antd";
 
 const SideNavbar = () => {
   const [activeItem, setActiveItem] = useState(0);
@@ -27,49 +24,60 @@ const SideNavbar = () => {
       <h1 className="text-xl font-bold text-black">StoreTrack</h1>
       <div className="h-full flex flex-col justify-between ">
         <div className="flex flex-col space-y-4">
-          <div className="flex w-full justify-between border-2 border-gray-400 p-2 rounded-md bg-gray-200 ">
-            <div className="flex justify-center items-center space-x-1">
-              <IoStorefront />
-              <span className="text-sm font-medium">OwnShop</span>
+          <div className="flex w-full justify-between border-2 border-gray-300 p-2 rounded-md bg-gray-200 ">
+            <div className="w-full flex justify-between items-center space-x-3">
+              <IoStorefront size={25} />
+              <Select
+                className="w-full shadow-sm rounded-md"
+                defaultValue={"ownshop"}
+                options={[
+                  { value: "ownshop", label: "OwnShop" },
+                  { value: "secondshop", label: "SecondShop" },
+                  { value: "thirdshop", label: "ThirdShop" },
+                ]}
+              />
             </div>
-            <button>
-              <RiExpandUpDownLine />
-            </button>
           </div>
-          <nav className="flex w-full flex-col space-y-2 justify-start items-start">
+          <div>
             <span className="text-sm">MENU</span>
-            <ul className="flex flex-col w-full space-y-1">
-              {menuItems.map((item, index) => (
-                <Link to={item.path}>
-                  <li
-                    key={index}
-                    onClick={() => setActiveItem(index)}
-                    className={`
-              group flex items-center
+            <nav className="w-full h-fit flex flex-col space-y-2 justify-start items-start">
+              <ul className="flex flex-col w-full space-y-1">
+                {menuItems.map((item, index) => (
+                  <Link to={item.path}>
+                    <li
+                      key={index}
+                      onClick={() => setActiveItem(index)}
+                      className={`
+              flex items-center
               p-2 rounded-md transition-all
               hover:bg-gray-100
-              ${activeItem == index ? "bg-gray-100 border-2 border-slate-300" : ""}
-              text-sm md:text-base cursor-pointer
+              ${
+                activeItem == index
+                  ? "bg-gray-100 border-2 border-slate-300"
+                  : ""
+              }
+               cursor-pointer
             `}
-                  >
-                    <item.icon className="w-5 h-5 md:w-6 md:h-6 text-gray-600" />
-                    <span className="ml-3 whitespace-nowrap overflow-hidden text-ellipsis">
-                      {item.text}
-                    </span>
-                  </li>
-                </Link>
-              ))}
-            </ul>
-          </nav>
+                    >
+                      <item.icon size={20} />
+                      <span className="ml-3 text-sm lg:text-md whitespace-nowrap overflow-hidden text-ellipsis">
+                        {item.text}
+                      </span>
+                    </li>
+                  </Link>
+                ))}
+              </ul>
+            </nav>
+          </div>
         </div>
         <div className="w-full flex flex-col space-y-2 bg-gray-200 p-2 rounded-lg border-2 border-gray-300 shadow-sm">
-          <div className="flex justify-start items-center space-x-2 border-b border-gray-300 py-2">
+          <div className="w-full flex justify-start items-center space-x-2 border-b border-gray-300 py-2">
             <img
               src={ownerImage}
               alt="the image of the owner account "
               className="w-8 h-8 rounded-lg"
             />
-            <address className="w-fit text-xs flex flex-col not-italic">
+            <address className="w-fit text-xs flex flex-col not-italic overflow-hidden whitespace-nowrap text-ellipsis">
               <p className="font-medium">Oussama Deghfel</p>
               <span className="text-gray-500">oussama@gmail.com</span>
             </address>
