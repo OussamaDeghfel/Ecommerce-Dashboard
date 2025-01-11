cd28f3fb2da3257c2e49535d9e9693c7aee8fbf5
OussamaDeghfel <oussama.deghfel@gmail.com>
2025-01-11 21:24:04 +0100

    enh: add the statistic of antd

--- src/pages/home/performance.tsx
+++ src/pages/home/performance.tsx
@@ -1,4 +1,5 @@
-import { FaArrowDown, FaArrowUp } from "react-icons/fa";
+import { ArrowDownOutlined, ArrowUpOutlined } from "@ant-design/icons";
+import { Statistic } from "antd";
 
 const Performance = () => {
   return (
@@ -26,21 +27,31 @@ const Performance = () => {
         <div className="flex justify-between items-center border-2 border-gray-300 p-5 rounded-md">
           <div className="flex flex-col space-y-2">
             <span className="text-base">Total Views</span>
-            <span className="text-2xl font-medium">21,040</span>
+            <span className="text-3xl font-medium">21,040</span>
             <span className="text-base">From last 15,542</span>
           </div>
-          <div className="bg-green-300 rounded-md w-fit h-fit flex items-center space-x-2 p-2">
-            <FaArrowUp /> <span>6040</span>
+          <div className="bg-green-300 rounded-md w-fit h-10 flex items-center space-x-2 p-2">
+            <Statistic
+              value={432}
+              precision={0}
+              valueStyle={{ color: "#3f8600", fontSize: "1rem" }}
+              prefix={<ArrowUpOutlined size={10} />}
+            />
           </div>
         </div>
         <div className="flex justify-between items-center  space-y-2 border-2 border-gray-300 p-5 rounded-md">
           <div className="flex flex-col space-y-2">
             <span className="text-base">Visits</span>
-            <span className="text-2xl font-medium">5,985</span>
+            <span className="text-3xl font-medium">5,985</span>
             <span className="text-base">From last 2,002</span>
           </div>
           <div className="bg-green-300 rounded-md w-fit h-fit flex items-center space-x-2 p-2">
-            <FaArrowUp /> <span>3001</span>
+            <Statistic
+              value={3001}
+              precision={0}
+              valueStyle={{ color: "#3f8600", fontSize: "1rem" }}
+              prefix={<ArrowUpOutlined size={10} />}
+            />
           </div>
         </div>
         <div className="flex justify-between items-center space-y-2 border-2 border-gray-300 p-5 rounded-md">
@@ -50,7 +61,12 @@ const Performance = () => {
             <span className="text-base">From last 560</span>
           </div>
           <div className="bg-green-300 rounded-md w-fit h-fit flex items-center space-x-2 p-2">
-            <FaArrowUp /> <span>516</span>
+            <Statistic
+              value={516}
+              precision={0}
+              valueStyle={{ color: "#3f8600", fontSize: "1rem" }}
+              prefix={<ArrowUpOutlined size={10} />}
+            />
           </div>
         </div>
         <div className="flex justify-between items-center space-y-2 border-2 border-gray-300 p-5 rounded-md">
@@ -59,8 +75,14 @@ const Performance = () => {
             <span className="text-2xl font-medium">85%</span>
             <span className="text-base">From last 90%</span>
           </div>
-          <div className="bg-red-300 rounded-md w-fit h-fit flex items-center space-x-2 p-2">
-            <FaArrowDown /> <span>5%</span>
+          <div className="bg-red-200  rounded-md w-fit h-fit flex items-center space-x-2 p-2">
+            <Statistic
+              value={5}
+              precision={0}
+              valueStyle={{ color: "#ff0000", fontSize: "1rem" }}
+              prefix={<ArrowDownOutlined size={10} />}
+              suffix={"%"}
+            />
           </div>
         </div>
       </div>
